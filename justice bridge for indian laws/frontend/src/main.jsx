22e44dd4c990eca86f87c1333b6ae79e615f3b3f
madhuri-badeti <madhuribadeti2006@gmail.com>
2026-09-10 00:21:36 +0530

    Update main.jsx

--- justice bridge for indian laws/frontend/src/main.jsx
+++ justice bridge for indian laws/frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/Justice-Bridge-For-Indian-Laws">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
